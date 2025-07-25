d83a43dda40f2f09cbd411b228a26d49f6497c45
leul1992 <leulbekele191@gmail.com>
2025-07-25 21:24:39 +0300

    update firebase config

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,12 +3,11 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as admin from 'firebase-admin';
 
+import serviceAccount from '../serviceAccountKey.json';
+import { ServiceAccount } from 'firebase-admin';
+
 admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-  }),
+  credential: admin.credential.cert(serviceAccount as ServiceAccount),
 });
 
 async function bootstrap() {
